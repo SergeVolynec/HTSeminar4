cc39bfaedb2aed308c05fa060cd3f9ef09558f08
Serge Volynets <sergej-volynec@yandex.ru>
2023-02-22 18:27:04 +0300

    HTSeminar4

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача1
+// Console.WriteLine("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int result = 1;
+// for (int i=1;i<=b;i++)
+// {
+//     result = result*a; 
+// }
+// Console.WriteLine($"Число {a} в степени {b} равняется {result}");
+
+//Задача2
+// Console.WriteLine("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// while (a > 0)
+// {
+//     sum+=a%10;
+//     a = a/10;
+// }
+// Console.WriteLine(sum);
+
+//
+
+//Задача3
+int [] arr = new int [8];
+for (int i = 0; i < 8; i++)
+{
+    Console.WriteLine($"Введите {i+1}-ый элемент массива: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(String.Join(';', arr));
